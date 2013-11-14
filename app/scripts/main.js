8ed5a53f623ec3d0080e59511d22c91a4331f7e0
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-11-13 22:00:48 -0500

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+require.config({
+    paths: {
+        // jquery: '../bower_components/jquery/jquery',
+        lodash: '../bower_components/lodash/dist/lodash',
+        d3: '../bower_components/d3/d3',
+        bacon: '../bower_components/bacon/dist/Bacon'
+    },
+
+    shim: {
+            d3: {
+                    exports: 'd3'
+            }
+    }
+});
+
+require([], function () {
+    'use strict';
+    console.log('yo');
+});
+
